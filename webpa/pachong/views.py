39bbae9dc61b240197dbcb9fc80fdcbfb80fefd4
guocq <guocqmail@163.com>
2016-09-09 11:17:19 +0800

    给web 添加login页面 和相关的static 文件

--- webpa/pachong/views.py
+++ webpa/pachong/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def index(request):
-    return HttpResponse('hi!!')
+    return HttpResponse('hi2!!')
